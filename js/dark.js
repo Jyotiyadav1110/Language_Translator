9dfefae57ecbaf2f3888d7b4cdd121370266e545
Jyoti Yadav <82959912+Jyotiyadav1110@users.noreply.github.com>
2022-12-17 16:45:35 +0530

    Add files via upload
    countries.js dark.js dcrit.js

---
+++ js/dark.js
@@ -0,0 +1,15 @@
+// window.onload = function(){
+//     var body = document.getElementById('body');
+//     var button1 = document.querySelector(".button1");
+
+//     button1.onclick = function (){
+//         if(body.className == "black"){
+//             body.className = "white";
+//             button1.className = "black";
+//         }else{
+//             body.className = "black";
+//             button1.className = "white";
+//         }
+//     }
+// }
+
